668afcc48d5488a5b076edfc07fec89907d534ec
farouk <faiz@eurecom.fr>
2020-08-11 01:42:47 +0200

    Disable setuid sandbox

--- scraper.js
+++ scraper.js
@@ -12,7 +12,9 @@ const fetchUrl = async (url) => {
 
   // The content of the page is dynamically generated
   // Launch in no sandbox mode for heroku deployment
-  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
   await page.goto(newUrl);
   await page.waitFor(1000);
